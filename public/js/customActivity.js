dfce8928dad345fba1cb616514b11f5e30067437
Vernika <vernika.1294@gmail.com>
2019-03-28 00:37:03 +0530

    api fixed

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -112,7 +112,7 @@ define([
         xhr.setRequestHeader("Content-Type","application/json",true);
 
 
-        xhr.setRequestHeader("Access-Control-Allow-Origin","*" );
+        xhr.setRequestHeader("Access-Control-Allow-Origin","https://mobile.useinsider.com/api/v1/notification/user" );
         xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
         xhr.setRequestHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         xhr.setRequestHeader("Access-Control-Max-Age", "3600");
@@ -122,7 +122,8 @@ define([
 
         xhr.onreadystatechange = function(e){
             console.log(xhr.status);
-            console.log(xhr.readyState);    
+            console.log(xhr.readyState);
+            console.log(xhr.responseText);    
         };
         xhr.send(jsonObj);
         
