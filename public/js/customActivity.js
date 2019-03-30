416cf44bead01adb788a447998877102085d9d6d
Vernika <vernika.1294@gmail.com>
2019-03-30 18:48:14 +0530

    form updated

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -9,7 +9,8 @@ define([
         var authTokens = {};
         var payload = {};
         $(window).ready(onRender);
-
+        const MongoClient = require("mongodb").MongoClient;
+                    
         connection.on('initActivity', initialize);
         connection.on('requestedTokens', onGetTokens);
         connection.on('requestedEndpoints', onGetEndpoints);
@@ -140,7 +141,32 @@ define([
                 console.log(xhr.readyState);
                 console.log(xhr.responseText);
                 if (xhr.readyState == 4) {
-                    const MongoClient = require("mongodb").MongoClient;
+                    /*
+                    var mongodb = require('mongodb');
+                    //We need to work with "MongoClient" interface in order to connect to a mongodb server.
+                    var MongoClient = mongodb.MongoClient;
+
+                    // Connection URL. This is where your mongodb server is running.
+
+                    //(Focus on This Variable)
+                    var url = 'mongodb://localhost:27017/my_database_name';      
+                    //(Focus on This Variable)
+
+                    // Use connect method to connect to the Server
+                    MongoClient.connect(url, function (err, db) {
+                    if (err) {
+                        console.log('Unable to connect to the mongoDB server. Error:', err);
+                    } else {
+                        console.log('Connection established to', url);
+
+                        // do some work here with the database.
+
+                        //Close connection
+                        db.close();
+                    }
+                    });
+                    */ 
+                    //const MongoClient = require("mongodb").MongoClient;
                     const uri = "mongodb+srv://vernika_1295:Vernika@123@cluster0-xbfkm.mongodb.net/test?retryWrites=true";
                     const client = new MongoClient(uri, { useNewUrlParser: true });
                     client.connect(err => {
