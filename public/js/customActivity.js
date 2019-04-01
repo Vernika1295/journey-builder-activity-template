decf327a12e49f0067d7c2e8292a674a411034e0
Vernika <vernika.1294@gmail.com>
2019-04-01 10:32:14 +0530

    fixed json payload

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -61,7 +61,7 @@ define([
         }
 
         function save() {
-            var campaignId = $("#camp").val();
+            var campaignId = parseInt($("#camp").val());
             var title = $("#titl").val();
             var imageUrl = $("#imgurl").val();
             var message = $("#msg").val();
@@ -84,27 +84,6 @@ define([
             }
             //Generating json to send
             /*
-            var jsonObj={  
-                "api_key":apikey,
-                "notifications":[  
-                   {
-                      "target":{  
-                         "email":contactEmail,
-                         "email_key":emailKey
-                      },
-                      "title":title,
-                      "message":message,
-                      "deep_link":{  
-                         deepLinkKey:deepLinkVal
-                      },
-                      "image_URL":imageUrl,
-                      "android_sound":"Beep",
-                      "ios_sound":"Beep",
-                      "channel_id": channelId,
-                      "camp_id": campaignId
-                   }
-                ]
-             } */
             var jsonObj = {
                 "api_key": apikey,
                 "notifications": [
@@ -124,54 +103,33 @@ define([
                         "camp_id": campaignId
                     }
                 ]
-            }
+            } */
+            var jsonObj={};
+            jsonObj["api_key"]=apiKey;
+            jsonObj["notifications"]=[];
+            var notificationObj={};
+            notificationObj["target"]={};
+            notificationObj["target"][emailKey]=contactEmail;
+            notificationObj["title"]=title;
+            notificationObj["message"]=message;
+            notificationObj["deep_link"]={};
+            notificationObj["deep_link"][deepLinkKey]=deepLinkVal;
+            notificationObj["image_URL"]=imageUrl;
+            notificationObj["android_sound"]="Beep";
+            notificationObj["ios_sound"]="Beep";
+            notificationObj["channel_id"]=channelId;
+            notificationObj["camp_id"]=campaignId;
+            jsonObj["notifications"].push(notificationObj);
+
             console.log("Json structure: " + JSON.stringify(jsonObj));
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "https://cors-anywhere.herokuapp.com/https://mobile.useinsider.com/api/v1/notification/user", true);
             xhr.setRequestHeader("Content-Type", "application/json");
-            //xhr.setRequestHeader("Access-Control-Allow-Origin",'https://pushnotifyapp.herokuapp.com/' );
-            //xhr.setRequestHeader("Access-Control-Allow-Credentials", false);
-            //xhr.setRequestHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-            //xhr.setRequestHeader("Access-Control-Allow-Headers","origin, x-requested-with, content-type,Access-Control-Allow-Origin,content-encoding,Date,Transfer-Encoding,Connection,vary,x-envoy-upstream-service-time,Expect-CT,Server,CF-RAY");
             xhr.onreadystatechange = function (e) {
                 console.log(xhr.status);
                 console.log(xhr.readyState);
                 console.log(xhr.responseText);
                 if (xhr.readyState == 4) {
-                    /*
-                    var mongodb = require('mongodb');
-                    //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-                    var MongoClient = mongodb.MongoClient;
-
-                    // Connection URL. This is where your mongodb server is running.
-
-                    //(Focus on This Variable)
-                    var url = 'mongodb://localhost:27017/my_database_name';      
-                    //(Focus on This Variable)
-
-                    // Use connect method to connect to the Server
-                    MongoClient.connect(url, function (err, db) {
-                    if (err) {
-                        console.log('Unable to connect to the mongoDB server. Error:', err);
-                    } else {
-                        console.log('Connection established to', url);
-
-                        // do some work here with the database.
-
-                        //Close connection
-                        db.close();
-                    }
-                    });
-                    /* 
-                    const MongoClient = require("mongodb").MongoClient;
-                    const uri = "mongodb+srv://vernika_1295:Vernika@123@cluster0-xbfkm.mongodb.net/test?retryWrites=true";
-                    const client = new MongoClient(uri, { useNewUrlParser: true });
-                    client.connect(err => {
-                        const collection = client.db("test").collection("devices");
-                        // perform actions on the collection object
-                        console.log(collection);
-                        client.close();
-                    });*/
                     payload['arguments'].execute.inArguments = [{
                         "tokens": authTokens,
                         "EmailAddress": "{{Contact.Attribute.Contact_Extension.EmailAddress}}"
